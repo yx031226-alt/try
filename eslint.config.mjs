3e2ef3f5596f1fff32b418b4140277b2cd6908f6
泡泡龙 <2731947957@qq.com>
2026-07-18 22:03:57 +0800

    build: initialize TypeScript workspace

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['**/dist/**', '**/node_modules/**', '**/db/**', '**/snapshots/**'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+);
